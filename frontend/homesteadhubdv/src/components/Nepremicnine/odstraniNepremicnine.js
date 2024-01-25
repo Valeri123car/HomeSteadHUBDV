89947fd9bc03f2de57eb1e2b879512d5011f5bca
Valeri123car <valeri.kamburov@student.um.si>
2024-01-25 18:31:22 +0100

    vzpostavitev reacta #HHDV-57 DONE Time 1h

--- frontend/homesteadhubdv/src/components/Nepremicnine/odstraniNepremicnine.js
+++ frontend/homesteadhubdv/src/components/Nepremicnine/odstraniNepremicnine.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Navbar from "../Navbar/Navbar";
 import Button from "@mui/material/Button";
 import { useParams, Link } from "react-router-dom";
-import "../../css/odstraniNepremicnino.css"; 
+import "../../css/odstraniNepremicnino.css";
 
 const NepremicnineApp = () => {
   const [data, setData] = useState([]);
@@ -29,7 +29,6 @@ const NepremicnineApp = () => {
     axios
       .delete(`http://localhost:8080/api/v1/nepremicnine/${id}`)
       .then(() => {
-
         fetchData();
       })
       .catch((error) => {
@@ -53,7 +52,6 @@ const NepremicnineApp = () => {
         updateData
       )
       .then(() => {
-
         fetchData();
 
         setUpdateData({
@@ -80,70 +78,85 @@ const NepremicnineApp = () => {
   };
 
   return (
-    <><Navbar />
-    <div class="okence1">
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Naziv</th>
-            <th>Opis</th>
-            <th>Cena</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((nepremicnina) => (
-            <tr key={nepremicnina.idNepremicnine}>
-              <td>{nepremicnina.idNepremicnine}</td>
-              <td>{nepremicnina.naziv}</td>
-              <td>{nepremicnina.opis}</td>
-              <td>{nepremicnina.cena}</td>
-              <td>
-                <Link to={`/ogledNepremicnine/${nepremicnina.idNepremicnine}`}>
-                  View
-                </Link>{" "}
-                <button onClick={() => handleUpdate(nepremicnina)}>
-                  Update
-                </button>{" "}
-                <button onClick={() => handleDelete(nepremicnina.idNepremicnine)}>
-                  Delete
-                </button>
-              </td>
+    <>
+      <Navbar />
+      <div class="okence1">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Naziv</th>
+              <th>Opis</th>
+              <th>Cena</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <div>
-        <h2>Update Nepremicnina</h2>
-        <form>
-          <label>ID:</label>
-          <input type="text" value={updateData.idNepremicnine} readOnly />
+          </thead>
+          <tbody>
+            {data.map((nepremicnina) => (
+              <tr key={nepremicnina.idNepremicnine}>
+                <td>{nepremicnina.idNepremicnine}</td>
+                <td>{nepremicnina.naziv}</td>
+                <td>{nepremicnina.opis}</td>
+                <td>{nepremicnina.cena}</td>
+                <td>
+                  <Link
+                    to={`/ogledNepremicnine/${nepremicnina.idNepremicnine}`}
+                  >
+                    View
+                  </Link>{" "}
+                  <button onClick={() => handleUpdate(nepremicnina)}>
+                    Update
+                  </button>{" "}
+                  <button
+                    onClick={() => handleDelete(nepremicnina.idNepremicnine)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div>
+          <h2>Update Nepremicnina</h2>
+          <form>
+            <label>ID:</label>
+            <input type="text" value={updateData.idNepremicnine} readOnly />
 
-          <label>Naziv:</label>
-          <input
-            type="text"
-            value={updateData.naziv}
-            onChange={(e) => setUpdateData({ ...updateData, naziv: e.target.value })} />
+            <label>Naziv:</label>
+            <input
+              type="text"
+              value={updateData.naziv}
+              onChange={(e) =>
+                setUpdateData({ ...updateData, naziv: e.target.value })
+              }
+            />
 
-          <label>Opis:</label>
-          <input
-            type="text"
-            value={updateData.opis}
-            onChange={(e) => setUpdateData({ ...updateData, opis: e.target.value })} />
+            <label>Opis:</label>
+            <input
+              type="text"
+              value={updateData.opis}
+              onChange={(e) =>
+                setUpdateData({ ...updateData, opis: e.target.value })
+              }
+            />
 
-          <label>Cena:</label>
-          <input
-            type="number"
-            value={updateData.cena}
-            onChange={(e) => setUpdateData({ ...updateData, cena: e.target.value })} />
+            <label>Cena:</label>
+            <input
+              type="number"
+              value={updateData.cena}
+              onChange={(e) =>
+                setUpdateData({ ...updateData, cena: e.target.value })
+              }
+            />
 
-          <button type="button" onClick={handleUpdateSubmit}>
-            Update Nepremicnina
-          </button>
-        </form>
+            <button type="button" onClick={handleUpdateSubmit}>
+              Update Nepremicnina
+            </button>
+          </form>
+        </div>
       </div>
-    </div></>
+    </>
   );
 };
 
